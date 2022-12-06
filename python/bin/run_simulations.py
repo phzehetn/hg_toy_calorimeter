e8a4bf5c523d254818c2ee517c8ee19580a56b2a
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2022-12-06 14:46:53 +0100

    feat: Make run_simulations.py script more generic

--- python/bin/run_simulations.py
+++ python/bin/run_simulations.py
@@ -1,5 +1,9 @@
+import configparser
 import json
 import multiprocessing
+import os
+import sys
+
 import numpy as np
 import argh
 import time
@@ -12,26 +16,10 @@ from calo.config import set_env, is_laptop
 set_env()
 
 
-def work(simtype):
+def work(simtype, output_path, num_events, num_events_per_file):
     # %%
     import minicalo
     set_env()
-
-    # if not part:
-    #     raise RuntimeError('Not implemented')
-    # output_path = '/eos/home-s/sqasim/Datasets/b4toys/run4/pu2' if not is_laptop else '../sample_pu/bid_f'
-    # output_path = '/eos/cms/store/cmst3/group/hgcal/CMG_studies/pepr/b4toys/run2/pu_raw/v1' if not is_laptop else '../sample_pu/bid_f'
-    output_path = '/eos/user/s/sqasim/Datasets/b4toys/run2/pu_raw/v1' if not is_laptop else '../sample_pu/with_fixed_kenergy'
-
-    if simtype=='singlepart':
-        output_path = '/eos/user/s/sqasim/Datasets/b4toys/run2/particles_raw/v1' if not is_laptop else '../sample_particles/v1'
-    elif simtype=='singlepart_face':
-        output_path = '/eos/user/s/sqasim/Datasets/b4toys/run2/particles_raw/v1face' if not is_laptop else '../sample_particles/v1t'
-    elif simtype=='minbias':
-        output_path = '/eos/user/s/sqasim/Datasets/b4toys/run2/pu_raw/v1' if not is_laptop else '../sample_pu/1'
-    elif simtype=='qqbar2ttbar':
-        output_path = '/eos/user/s/sqasim/Datasets/b4toys/run2/qqbar2ttbar_raw/v1' if not is_laptop else '../sample_qqbar2ttbar/3'
-
 
     np.random.seed()
     rnd = np.random.randint(0, 1000000000)
@@ -51,9 +39,13 @@ def work(simtype):
     if simtype == 'singlepart':
         particle_pdgid += [15]
     particle_generator = ParticleGenerator(detector_specs_2, range_energy=[0.1, 200], particle_pdgid=particle_pdgid)
-    for i in range(400):
+
+    num_events_written = 0
+    while True:
+        if num_events_written==num_events:
+            break
         simulation_results_array = []
-        for j in range(5 if is_laptop else 100):
+        for j in range(num_events_per_file):
             if simtype in {'singlepart', 'singlepart_face'}:
                 particle = particle_generator.generate(from_iteraction_point=simtype=='singlepart')
                 simulation_result = minicalo.simulate_particle(
@@ -79,12 +71,29 @@ def work(simtype):
         dataset = ra_pickles.RandomAccessPicklesWriter(len(simulation_results_array), output_path)
         for x in simulation_results_array:
             dataset.add(x)
+            num_events_written += 1
         dataset.close()
 
 
 
-def main(simtype, cores=10):
-    allowed_sim_types = {'minbias', 'qqbar2ttbar', 'multipart', 'singlepart', 'singlepart_face'}
+def main(simtype, output_path, cores=10, num_simulations=1000000, num_simulations_per_file=100):
+    """
+    Generate a large set of simulations using multiple CPU/CPU cores
+
+    :param simtype: Type of the simulation. Can be either of the following:
+                    1. minbias -- 14 TeV proton-proton interactions
+                    2. qqbar2ttbar -- qqbar to ttbar interaction
+                    3. singlepart -- generate single particles at the interaction point with 1.4 |eta| < 3.1
+                    4. singlepart_face -- generate single particles with 1.4 |eta| < 3.1, 1 cm away from the calorimeter
+    :param output-path: A folder where to store the simulations, will be created if it does not exist.
+    :param cores: Number of CPU cores
+    :param num-simulations: Number of events to generate per CPU core
+    :param num_simulations_per_file: Number of events to generate per file per CPU core (just a storage spec)
+    """
+
+    os.system('mkdir -p %s'%output_path)
+
+    allowed_sim_types = {'minbias', 'qqbar2ttbar', 'singlepart', 'singlepart_face'}
     if not simtype in allowed_sim_types:
         raise ValueError('Wrong simtype %s: select from '%simtype, allowed_sim_types)
 
@@ -93,7 +102,7 @@ def main(simtype, cores=10):
     processes = []
     for m in range(cores):
         print("Starting")
-        p = multiprocessing.Process(target=work, args=(simtype,))
+        p = multiprocessing.Process(target=work, args=(simtype,output_path, num_simulations, num_simulations_per_file))
         p.start()
         time.sleep(0.3)
         processes.append(p)
@@ -102,5 +111,21 @@ def main(simtype, cores=10):
         p.join()
 
 if __name__=='__main__':
-    argh.dispatch_command(main)
+    is_config_file = np.any(['.ini' in x for x in sys.argv])
+    if is_config_file:
+        config_file = sys.argv[1]
+        section = sys.argv[2]
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        simtype=config[section]['simtype']
+        outpath=config[section]['outpath']
+        cores=config[section]['cores']
+
+        main(simtype, outpath, int(cores))
+    else:
+        argh.dispatch_command(main)
+
+
+
 
